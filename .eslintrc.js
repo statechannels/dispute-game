eb1fe26c1822a3296fad30f69eab1d91f6762637
Alex Gap <alex.gap@gmail.com>
2021-09-17 11:25:03 -0700

    fix identation

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,18 @@
 module.exports = {
-    parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint', 'prettier', 'import'],
-    extends: [
-        'eslint:recommended',
-        'plugin:prettier/recommended',
-        'plugin:import/errors',
-        'plugin:import/warnings',
-        'plugin:import/typescript'
-    ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier', 'import'],
+  extends: [
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript'
+  ],
 
-    overrides: [{
-        files: ['**/*.ts'],
-        extends: ['plugin:@typescript-eslint/recommended']
-    }]
+  overrides: [
+    {
+      files: ['**/*.ts'],
+      extends: ['plugin:@typescript-eslint/recommended']
+    }
+  ]
 };
