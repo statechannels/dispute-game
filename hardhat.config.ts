f7ea65818b1f9ccf73bf3dd3d1877f18b11452be
Alex Gap <alex.gap@gmail.com>
2021-09-21 10:05:00 -0700

    Generate and store types

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,12 +1,18 @@
 import {HardhatUserConfig} from 'hardhat/types';
 
 import '@nomiclabs/hardhat-waffle';
+import '@nomiclabs/hardhat-ethers';
 import '@typechain/hardhat';
 
 const config: HardhatUserConfig = {
+  paths: {tests: 'contract-tests'},
   defaultNetwork: 'hardhat',
   solidity: {
     compilers: [{version: '0.8.7', settings: {}}]
+  },
+  typechain: {
+    outDir: 'src/contract-types',
+    target: 'ethers-v5'
   }
 };
 
