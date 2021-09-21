1266f84110f10a52d75ce9fd3dfe2182b8666828
Mike Kerzhner <mikerzhner@gmail.com>
2021-09-21 11:55:46 -0600

    test: collect coverage only for ChallengeManager

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,10 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  collectCoverageFrom: ['src/**/*.{js,ts}']
+  /**
+   * Only collect coverage for ChallengeManager and utilities used by ChallengeManager
+   * merkle.ts IS a utility used by challenge manager. But this utility is fairly specific
+   *  to merkle-tools dependency. So it is ignored for now.
+   */
+  collectCoverageFrom: ['src/**/*.{js,ts}', '!src/{index,merkle,auto-disputer}.ts']
 };
