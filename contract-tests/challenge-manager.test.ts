34123f65fcec83a7ef42a7cae98383a374837d6f
Alex Gap <alex.gap@gmail.com>
2021-10-03 18:21:27 -0700

    Basic challenge manager

---
+++ contract-tests/challenge-manager.test.ts
@@ -0,0 +1,42 @@
+import {ethers} from 'hardhat';
+import {ChallengeManager} from '..//src/contract-types/ChallengeManager';
+
+import {generateWitness, hash} from '../src/merkle';
+import _ from 'lodash';
+
+function getElements(array: string[], indices: number[]): string[] {
+  return indices.map(i => array[i]);
+}
+
+describe('Challenge Manager Contract', () => {
+  let manager: ChallengeManager;
+
+  it('can perform a basic bisection', async () => {
+    const merkleUtils = await (await ethers.getContractFactory('MerkleUtils')).deploy();
+
+    const factory = await ethers.getContractFactory('ChallengeManager', {
+      libraries: {MerkleUtils: merkleUtils.address}
+    });
+
+    const correctStates = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const correctHashes = correctStates.map(num => hash(num.toString()));
+    const incorrectStates = [0, 1, 2, 3, 4, 5.1, 6.1, 7.1, 8.1, 9.1];
+    const incorrectHashes = incorrectStates.map(num => hash(num.toString()));
+
+    manager = await factory.deploy(getElements(correctHashes, [0, 4, 9]), 9, 'challenger', 2);
+
+    await manager.split(
+      generateWitness(getElements(correctHashes, [0, 4, 9]), 1),
+      getElements(incorrectHashes, [6, 9]),
+      generateWitness(getElements(correctHashes, [0, 4, 9]), 2),
+      'proposer'
+    );
+
+    await manager.split(
+      generateWitness(getElements(incorrectHashes, [4, 6, 9]), 1),
+      getElements(correctHashes, [5, 6]),
+      generateWitness(getElements(incorrectHashes, [4, 6, 9]), 2),
+      'challenger'
+    );
+  });
+});
